80cf6451b0f5c50bd835a2c279e0d6d0343cb954
Rob Ribeiro <azurelogic@gmail.com>
2017-09-15 10:10:50 -0400

    release script now signs

--- release.sh
+++ release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+npm config set sign-git-tag true
 npm version $1
 VERSION_TAG=`node -p "require('./package.json').version"`
-git tag $VERSION_TAG
+git tag -s $VERSION_TAG
 git push origin $VERSION_TAG
 npm publish
 
